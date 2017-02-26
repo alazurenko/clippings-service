7831cb14f21b591d699515d71e61c6baa139e4c5
Andrii Lazurenko <andrey.lazurenko@gmail.com>
2017-02-26 09:14:05 +0100

    Fix dependecies

--- src/clippingsRouter.js
+++ src/clippingsRouter.js
@@ -4,9 +4,17 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const parseData = require('./parsingService');
-const outputPath = `../uploads/output.txt`;
+const folder = `uploads`;
+const outputPath = `./${ folder }/output.txt`;
+
+function makeFolder(path) {
+  if(!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+  }
+}
 
 function handleFile(req, res, next) {
+  makeFolder(folder);
   let stream = fs.createWriteStream(outputPath);
   req.on('data', chunk => stream.write(chunk));
   req.on('end', next);
